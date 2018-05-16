35d0d2cb954c33e1d4a0b96e70fba23840bec974
Shunsuke Sato <shunsuke.sato26@gmail.com>
2018-05-16 11:55:27 +0200

    Added one-body density initialization.

--- src/initial_state.f90
+++ src/initial_state.f90
@@ -9,6 +9,7 @@ subroutine initial_state
 
     call allocate_manybody_wf
     call init_manybody_wf
+    call init_onebody_density
 
   case default
     write(message(1),"(I7)")sampling_method
@@ -46,6 +47,42 @@ subroutine allocate_manybody_wf
   end select
 
 end subroutine allocate_manybody_wf
+!-----------------------------------------------------------------------------------------
+subroutine init_onebody_density
+  use global_variables
+  implicit none
+  integer :: ix
+  real(8) :: dV
+
+  select case(total_particle_num)
+  case(2)
+
+! compute density for the first spieces
+    if(num_species == 1)then
+      dV = product(spec(1)%dx(:))
+    else if(num_species == 2)then
+      dV = product(spec(2)%dx(:))
+    end if
+
+    do ix = 1,spec(1)%ngrid_tot
+      spec(1)%rho_ini(ix) = sum(abs(zwfn_ini_2p(ix,:))**2)*dV
+    end do
+
+    if(num_species == 2)then
+      dV = product(spec(1)%dx(:))
+      do ix = 1,spec(2)%ngrid_tot
+        spec(2)%rho_ini(ix) = sum(abs(zwfn_ini_2p(:,ix))**2)*dV
+      end do
+    end if
+
+  case default
+    write(message(2),"(I7)")total_particle_num
+    message(1) = 'Fatal Error: init_onebody_density is not implemented for'
+    message(2) = '           : total_particle_num = '//trim(message(2))
+    call error_finalize(message(1:2))
+  end select
+
+end subroutine init_onebody_density
 !-----------------------------------------------------------------------------------------
 subroutine init_manybody_wf
   use global_variables
