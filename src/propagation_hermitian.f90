f2ac7bbdc50125f73d2635dbb34189d12c1b120f
Shunsuke Sato <shunsuke.sato26@gmail.com>
2018-05-21 12:55:57 +0200

    Implemented Runge-Kutta.

--- src/propagation_hermitian.f90
+++ src/propagation_hermitian.f90
@@ -9,7 +9,7 @@ subroutine propagation_hermitian
     if(mod(itraj-1,comm_nproc_global) /= comm_id_global)cycle
     do it = 1, num_time_step
 
-
+      call dt_evolve_Runge_Kutta4_hermitian
 
     end do
   end do
@@ -28,6 +28,7 @@ subroutine dt_evolve_Runge_Kutta4_hermitian
   end type species_temporal
 
   type(species_temporal) :: spec_t(num_species)
+  integer :: irk
 
   do ispec = 1, num_species
     allocate(spec_t(ispec)%zwfn(spec(ispec)%ngrid_tot,spec(ispec)%nparticle, 0:4))
@@ -36,16 +37,135 @@ subroutine dt_evolve_Runge_Kutta4_hermitian
     allocate(spec_t(ispec)%v_pt(spec(ispec)%ndim,spec(ispec)%nparticle))
   end do
 
-! RK1
+! RK1 ==========================================================================
+  irk = 1
   do ispec = 1, num_species
     spec_t(ispec)%zwfn(:,:, 0) = spec(ispec)%zwfn(:,:)
-    spec_t(ispec)%r_p(:,:, 0) = spec(ispec)%r_particle(:,:)
+    spec_t(ispec)%r_p(:,:, 0)  = spec(ispec)%r_particle(:,:)
   end do
 
   call calc_velocity_hermitian
   call calc_veff_hermitian
 
+  do ispec = 1, num_species
+    do ip = 1, spec(ispec)%nparticle
+      call laplacian(spec_t(ispec)%zwfn(:,ip,0), &
+                     spec_t(ispec)%zwfn(:,ip,irk), &
+                     spec(ispec)%ngrid,          &
+                     spec(ispec)%dx,             &
+                     -0.5d0)
+      spec_t(ispec)%zwfn(:,ip,irk) = -zI*(&
+          spec_t(ispec)%zwfn(:,ip,irk) &
+        + spec_t(ispec)%veff(:,ip)*spec_t(ispec)%zwfn(:,ip,0) )
 
+      spec_t(ispec)%r_p(:,ip,irk) = spec_t(ispec)%v_pt(:,ip)
+
+    end do
+  end do
+
+! RK2 ==========================================================================
+  irk = 2
+  do ispec = 1, num_species
+    spec_t(ispec)%zwfn(:,:, 0) = spec(ispec)%zwfn(:,:) &
+                               + 0.5d0*time_step*spec_t(ispec)%zwfn(:,:,1)
+    spec_t(ispec)%r_p(:,:, 0)  = spec(ispec)%r_particle(:,:) &
+                               + 0.5d0*time_step*spec_t(ispec)%r_p(:,:, 1)
+  end do
+
+  call calc_velocity_hermitian
+  call calc_veff_hermitian
+
+  do ispec = 1, num_species
+    do ip = 1, spec(ispec)%nparticle
+      call laplacian(spec_t(ispec)%zwfn(:,ip,0), &
+                     spec_t(ispec)%zwfn(:,ip,irk), &
+                     spec(ispec)%ngrid,          &
+                     spec(ispec)%dx,             &
+                     -0.5d0)
+      spec_t(ispec)%zwfn(:,ip,irk) = -zI*(&
+          spec_t(ispec)%zwfn(:,ip,irk) &
+        + spec_t(ispec)%veff(:,ip)*spec_t(ispec)%zwfn(:,ip,0) )
+
+      spec_t(ispec)%r_p(:,ip,irk) = spec_t(ispec)%v_pt(:,ip)
+
+    end do
+  end do
+
+
+! RK3 ==========================================================================
+  irk = 3
+  do ispec = 1, num_species
+    spec_t(ispec)%zwfn(:,:, 0) = spec(ispec)%zwfn(:,:) &
+                               + 0.5d0*time_step*spec_t(ispec)%zwfn(:,:,2)
+    spec_t(ispec)%r_p(:,:, 0)  = spec(ispec)%r_particle(:,:) &
+                               + 0.5d0*time_step*spec_t(ispec)%r_p(:,:, 2)
+  end do
+
+  call calc_velocity_hermitian
+  call calc_veff_hermitian
+
+  do ispec = 1, num_species
+    do ip = 1, spec(ispec)%nparticle
+      call laplacian(spec_t(ispec)%zwfn(:,ip,0), &
+                     spec_t(ispec)%zwfn(:,ip,irk), &
+                     spec(ispec)%ngrid,          &
+                     spec(ispec)%dx,             &
+                     -0.5d0)
+      spec_t(ispec)%zwfn(:,ip,irk) = -zI*(&
+          spec_t(ispec)%zwfn(:,ip,irk) &
+        + spec_t(ispec)%veff(:,ip)*spec_t(ispec)%zwfn(:,ip,0) )
+
+      spec_t(ispec)%r_p(:,ip,irk) = spec_t(ispec)%v_pt(:,ip)
+
+    end do
+  end do
+
+
+! RK4 ==========================================================================
+  irk = 4
+
+  do ispec = 1, num_species
+    spec_t(ispec)%zwfn(:,:, 0) = spec(ispec)%zwfn(:,:) &
+                               + time_step*spec_t(ispec)%zwfn(:,:,3)
+    spec_t(ispec)%r_p(:,:, 0)  = spec(ispec)%r_particle(:,:) &
+                               + time_step*spec_t(ispec)%r_p(:,:, 3)
+  end do
+
+  call calc_velocity_hermitian
+  call calc_veff_hermitian
+
+  do ispec = 1, num_species
+    do ip = 1, spec(ispec)%nparticle
+      call laplacian(spec_t(ispec)%zwfn(:,ip,0), &
+                     spec_t(ispec)%zwfn(:,ip,irk), &
+                     spec(ispec)%ngrid,          &
+                     spec(ispec)%dx,             &
+                     -0.5d0)
+      spec_t(ispec)%zwfn(:,ip,irk) = -zI*(&
+          spec_t(ispec)%zwfn(:,ip,irk) &
+        + spec_t(ispec)%veff(:,ip)*spec_t(ispec)%zwfn(:,ip,0) )
+
+      spec_t(ispec)%r_p(:,ip,irk) = spec_t(ispec)%v_pt(:,ip)
+
+    end do
+  end do
+
+!! Sum
+  do ispec = 1, num_species
+    spec(ispec)%zwfn(:,:) = spec_t(ispec)%zwfn(:,:, 0) +time_step/6d0*( &
+            spec_t(ispec)%zwfn(:,:, 1) &
+     +2d0 * spec_t(ispec)%zwfn(:,:, 2) &
+     +2d0 * spec_t(ispec)%zwfn(:,:, 3) &
+          + spec_t(ispec)%zwfn(:,:, 4) )
+
+    spec(ispec)%r_particle(:,:) = spec_t(ispec)%r_p(:,:,0) +time_step/6d0*( &
+            spec_t(ispec)%r_p(:,:,1) &
+     +2d0 * spec_t(ispec)%r_p(:,:,2) &
+     +2d0 * spec_t(ispec)%r_p(:,:,3) &
+          + spec_t(ispec)%r_p(:,:,4) )
+
+
+  end do
 
 
 
