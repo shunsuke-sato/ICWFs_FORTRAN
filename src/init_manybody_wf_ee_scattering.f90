72f8294351217271ae1f99ecb1d5cba954b7b688
Shunsuke Sato <shunsuke.sato26@gmail.com>
2018-05-24 18:50:43 +0200

    Implementing CG.

--- src/init_manybody_wf_ee_scattering.f90
+++ src/init_manybody_wf_ee_scattering.f90
@@ -1,6 +1,7 @@
 subroutine init_manybody_wf_ee_scattering
   use global_variables
   implicit none
+  real(8),allocatable :: veff(:)
   real(8),allocatable :: xvec(:),gvec(:),pvec(:)
   real(8),allocatable :: hxvec(:),hpvec(:)
   real(8) :: gg0, gg, xx, xhx, pp, php, xp, xhp
@@ -10,6 +11,16 @@ subroutine init_manybody_wf_ee_scattering
 
   nx = spec(1)%ngrid(1)
   dx = spec(1)%dx(1)
+
+  allocate(xvec(nx),pvec(nx),gvec(nx))
+  allocate(hxvec(nx),hpvec(nx))
+  allocate(veff(nx))
+
+  do ix = 1,nx
+    call random_double(xvec(ix))
+  end do
+
+  
 
 
 end subroutine init_manybody_wf_ee_scattering
